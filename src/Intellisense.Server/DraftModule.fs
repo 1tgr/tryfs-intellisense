15ae62df35f8c6d01e5faba158811ca6d996081a
Tim Robinson <tim.g.robinson@gmail.com>
2013-01-19 23:04:16 +0000

    REST routes

--- src/Intellisense.Server/DraftModule.fs
+++ src/Intellisense.Server/DraftModule.fs
@@ -2,11 +2,67 @@ namespace Tim.TryFSharp.Intellisense.Server
 
 open Nancy
 
-[<Url("/hello/{who}")>]
-type Hello = { Who : string } with interface IRestGet<string>
+[<Url("/{id}")>]
+type Draft = { Id : string } with
+    interface IRestDelete
+
+type Ref<'Resource> =
+    {
+        Url : string<'Resource>
+        Id : string
+    }
+
+type Drafts = | Drafts with
+    interface IRestGet<Ref<Draft> array>
+    interface IRestPost<unit, Ref<Draft>>
+
+type Token =
+    {
+        Todo : unit
+    }
+
+type ParseResult =
+    {
+        Tokens : Token array
+        Completions : string array
+    }
+
+[<Url("/{DraftId}/cell/{CellId}/tokens")>]
+type Tokens = { DraftId : string; CellId : string } with
+    interface IRestGet<Token array>
+
+[<Url("/{DraftId}/cell/{CellId}/completions")>]
+type Completions = { DraftId : string; CellId : string } with
+    interface IRestGet<string array>
+
+[<Url("/{DraftId}/cell/{CellId}/code")>]
+type Code = { DraftId : string; CellId : string } with
+    interface IRestGet<string>
+    interface IRestPut<string, ParseResult>
 
 type HelloModule() as t =
-   inherit NancyModule()
+   inherit NancyModule("/draft")
 
-   do t.GetT <| fun (hello : Hello) ->
-       sprintf "Hello, %s!" hello.Who
+   (*
+   do t.GetT <| fun Drafts ->
+       [|
+           { Url = t.UrlFor { Id = "a" }; Id = "a" }
+           { Url = t.UrlFor { Id = "b" }; Id = "b" }
+           { Url = t.UrlFor { Id = "c" }; Id = "c" }
+       |]
+   *)
+
+   do t.DeleteT <| fun (draft : Draft) ->
+       ()
+
+   do t.GetT <| fun (tokens : Tokens) ->
+       [| |]
+
+   do t.GetT <| fun (completions : Completions) ->
+       [| |]
+
+   do t.GetT <| fun (code : Code) ->
+       ""
+
+   do t.PutT <| fun (text : string) (code : Code) ->
+       { Tokens = [| |]; Completions = [| |] }
--- src/Intellisense.Server/nancy.fs
+++ src/Intellisense.Server/nancy.fs
@@ -1,8 +1,10 @@
+#nowarn "42"
 namespace Tim.TryFSharp.Intellisense.Server
 
 open System
 open Microsoft.FSharp.Reflection
 open Nancy
+open Nancy.ModelBinding
 
 type IRestResource =
     interface end
@@ -19,6 +21,9 @@ type IRestPost<'Request, 'Response> =
 type IRestDelete =
     inherit IRestResource
 
+[<MeasureAnnotatedAbbreviation>]
+type string<'Measure> = string
+
 [<AttributeUsage(AttributeTargets.Class, AllowMultiple = true)>]
 type UrlAttribute(template  : string) =
     inherit Attribute()
@@ -31,6 +36,9 @@ module NancyModuleExtensions =
     [<Sealed>]
     type private RestResourceImpl<'Resource> private () =
         static let typ = typeof<'Resource>
+        static let fieldNames = FSharpType.GetRecordFields(typ) |> Array.map (fun pi -> pi.Name)
+        static let ctor = FSharpValue.PreComputeRecordConstructor(typ)
+        static let reader = FSharpValue.PreComputeRecordReader(typ)
 
         static let urls =
             [|
@@ -38,25 +46,64 @@ module NancyModuleExtensions =
                     attr.Template
             |]
 
-        static let create : (string -> obj) -> 'Resource =
-            let fields = FSharpType.GetRecordFields(typ) |> Array.map (fun pi -> pi.Name)
-            let ctor = FSharpValue.PreComputeRecordConstructor(typ)
-            fun fieldFn ->
-                let values = Array.map fieldFn fields
-                unbox (ctor values)
-
         static member Urls = urls
-        static member Create = create
+
+        static member PartsToResource (part : string -> obj) : 'Resource =
+            let values = Array.map part fieldNames
+            unbox (ctor values)
+
+        static member ResourceToString (resource : 'Resource) : string =
+            match urls with
+            | [| |] -> failwithf "No [<Url>] attribute on %O" typeof<'Resource>
+            | a ->
+                let values = reader resource
+
+                String.concat "/" <| seq {
+                    for part in a.[0].Split('/') ->
+                        if part.StartsWith("<") && part.EndsWith(">") then
+                            let part = part.Substring(1, part.Length - 2)
+                            match Array.IndexOf(fieldNames, part) with
+                            | n when n >= 0 -> string (values.[n])
+                            | _ -> failwithf "No field called '%s' in %O" part typeof<'Resource>
+
+                        else
+                            part
+                }
+
 
     let private dynamicDictionaryField (dict : obj) (name : string) : obj =
         let dict : DynamicDictionary = unbox dict
         let value : DynamicDictionaryValue = unbox (dict.[name])
         value.Value
 
+    let private handler (fn : 'a -> 'b) : obj -> obj =
+        dynamicDictionaryField >> RestResourceImpl<_>.PartsToResource >> fn >> box
+
+    [<NoDynamicInvocation>]
+    let inline private retype (x : 'a) : 'b = (# "" x : 'b #)
+
     type NancyModule with
+        member t.UrlFor (resource : 'Resource) : string<'Resource> =
+            retype (RestResourceImpl<_>.ResourceToString(resource))
+
         member t.GetT<'Resource, 'Response when 'Resource :> IRestGet<'Response>> (fn : 'Resource -> 'Response) =
             for url in RestResourceImpl<'Resource>.Urls do
-                printfn "Registering route %s for %O" url typeof<'Resource>
-                t.Get.[url] <- fun parameters ->
-                    let resource = RestResourceImpl<'Resource>.Create (dynamicDictionaryField parameters)
-                    box (fn resource)
+                t.Get.[url] <- Func<_,_>(handler fn)
+
+        member t.PutT<'Resource, 'Request, 'Response when 'Resource :> IRestPut<'Request, 'Response>> (fn : 'Request -> 'Resource -> 'Response) =
+            for url in RestResourceImpl<'Resource>.Urls do
+                t.Put.[url] <- fun parameters ->
+                    let request = t.Bind<_>()
+                    handler (fn request) parameters
+
+        member t.PostT<'Resource, 'Request, 'Response when 'Resource :> IRestPost<'Request, 'Response>> (fn : 'Request -> 'Resource -> 'Response) =
+            for url in RestResourceImpl<'Resource>.Urls do
+                t.Post.[url] <- fun parameters ->
+                    let request = t.Bind<_>()
+                    handler (fn request) parameters
+
+        member t.DeleteT<'Resource when 'Resource :> IRestDelete> (fn : 'Resource -> unit) =
+            for url in RestResourceImpl<'Resource>.Urls do
+                t.Delete.[url] <- Func<_,_>(handler fn)
+
+
